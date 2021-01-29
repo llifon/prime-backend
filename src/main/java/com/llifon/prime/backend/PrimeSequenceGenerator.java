8909f31f29e607c156815568cecf6b772451f653
Llifon Osian Jones <llifon.o.jones@gmail.com>
2021-01-29 21:25:09 +0000

    Added the interface model for generating prime sequences
    Gone very generic for now - might be overkill but if we end up supporting >2 billion prime numbers, it's going to be useful

---
+++ src/main/java/com/llifon/prime/backend/PrimeSequenceGenerator.java
@@ -0,0 +1,26 @@
+package com.llifon.prime.backend;
+
+import java.util.Iterator;
+
+/**
+ * An interface for a unit that can generate a sequence of prime numbers which exist within a number range.
+ *
+ * @param <T> The data-type of the prime numbers. Useful if you want to customize the data type to store smaller
+ *            or larger numbers.
+ */
+public interface PrimeSequenceGenerator<T extends Number> {
+
+    /**
+     * Generates a sequence of PRIME NUMBERS which exist within a given value range.
+     *
+     * @param from      The first number in the range (always inclusive).
+     * @param upTo      The last number in the range (inclusiveness based on {@code inclusive} flag)
+     * @param inclusive If {@code true}, the {@code upTo} value will be included in the output
+     *                  if it itself is a prime number. Otherwise only PRIME NUMBERS which are less than
+     *                  the upper bound will be included.
+     * @return An iterator for all of the numbers that were generated. An iterator is chosen here in-order to
+     * support a sequence amount that's larger than the capacity of {@link Integer#MAX_VALUE} which could be
+     * an issue if we were to return an ArrayList for example.
+     */
+    Iterator<T> Generate(T from, T upTo, boolean inclusive);
+}
