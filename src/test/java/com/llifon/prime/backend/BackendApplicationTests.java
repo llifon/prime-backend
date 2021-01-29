dc20ab3e695846d111e3ec08a3442e3c38e5d2d0
Llifon Osian Jones <llifon.o.jones@gmail.com>
2021-01-29 21:06:17 +0000

    Default project configuration via IntelliJ
    Uses Spring Web, Spring Rest, Spring Data and Postgresql

---
+++ src/test/java/com/llifon/prime/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llifon.prime.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
